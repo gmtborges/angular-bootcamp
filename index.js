c6dd2bdbe44a6aa4226d6c1b4c3b38a231993c20
Gustavo Borges <gustavoborges@ibm.com>
2021-12-09 09:51:25 -0300

    fix cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let apiURL = 'https://swapi.co/api/people/';
 let xhr = new XMLHttpRequest;
 xhr.open('GET', apiURL, true);
+xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
 xhr.send();
 xhr.onload = function () {
     let xhrResponse =
@@ -8,6 +9,6 @@ xhr.onload = function () {
     console.log(xhrResponse);
 }
 
-fetch(apiURL)
+fetch(apiURL, { headers: { 'Access-Control-Allow-Origin': '*' } })
     .then(response => response.json())
     .then(data => console.log(data));
